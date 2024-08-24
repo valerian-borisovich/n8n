be48f012521d178366ac7258844e05a8df330c66
Valerian Borisovich <valerian.borisovich@gmail.com>
2024-08-25 01:51:28 +0200

    Update backup.sh

--- config/backup.sh
+++ config/backup.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
-
+# ###
+#
 echo "Backup started"
+
+# ###
+#
+CONFIG_DIR=/opt/render/project/src/config
+#N8N_CONFIG_DIR=/opt/render/.n8n/config
+
+# #########################################################
+# ###
+#
+if [ "$N8N_CONFIG_DIR" == '' ]; then
+	export N8N_CONFIG_DIR=/opt/render/.n8n/config
+fi
+
+echo -e "copy config"
+find $N8N_CONFIG_DIR -type f -name "*" -print 0| xargs -0 -r -I file cp -v -p file --target-directory=$CONFIG_DIR
+
+if [ "$CONFIG_DIR" == "$N8N_CONFIG_DIR" ]; then
+  echo -e 'config to github'
+	#git add /opt/render/project/src/config/*
+	git add $CONFIG_DIR/*
+	git commit -m "!date! !time!"
+	timeout /T 300
+fi
+
+# #########################################################
+
 
