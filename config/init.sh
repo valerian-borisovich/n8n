c7d3d962d0a96b02767697a83fc88d4d9484b229
Valerian <valerian.borisovich@gmail.com>
2024-09-19 07:00:38 +0200

    config 014

--- config/init.sh
+++ config/init.sh
@@ -1,10 +1,11 @@
-usr/bin/env bash
+#!/usr/bin/env bash
 # #########################################################################################################
 # ###
 #
 #set -euo pipefail
-
-
+#
+# ###
+#
 # ### store current directory
 # pushd .
 #
@@ -12,7 +13,10 @@ usr/bin/env bash
 # #########################################################################################################
 # ###
 #
-APP_ALLOWED_CONFIGS=.defaults,.config,.env,.ctx,.env.render
+if [ ! -d "$APP_ALLOWED_CONFIGS" ]; then
+  APP_ALLOWED_CONFIGS=.defaults,.config,.env,.ctx,.env.render
+  export APP_ALLOWED_CONFIGS
+fi
 
 # ###
 THIS=$(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null || echo $0)
@@ -53,10 +57,6 @@ echo -e "\033[0m"
 # #########################################################################################################
 # ###
 #
-
-# #########################################################################################################
-# ###
-#
 env_load() {
   echo -e "\033[38;2;0;255;02m"
   IFS=',' read -a arr <<<"$APP_ALLOWED_CONFIGS"
@@ -83,9 +83,7 @@ env_print() {
   # ###
   # env | sort >env.render.md 2>&1
 
-  # #########################################################################################################
-
-  echo -e "\033[38;2;0;255;02m"
+  echo -e "\033[38;2;0;255;32m"
   echo -e "   work directory: $PWD"
   echo "   APP_ variables:"
   env | sort | grep APP_
@@ -101,7 +99,6 @@ env_print() {
 # ###
 #
 
-# #########################################################################################################
 # ###   Config to github
 #
 config_push() {
@@ -124,22 +121,7 @@ config_push() {
 # ###   Config restore
 config_restore() {
   echo -e "   Config restore"
-
-  #if [ ! -d "$APP_CONFIG_DIR" ]; then mkdir -p "$APP_CONFIG_DIR"; fi
-
-  #if [ ! -d "$N8N_CONFIG_DIR" ]; then mkdir -p "$N8N_CONFIG_DIR"; fi
-  #cp -rf "$APP_CONFIG_DIR/." "$N8N_CONFIG_DIR"
-
-  #mkdir -p /opt/render/project/src/config
-  # cp -rf /opt/render/.n8n/* /opt/render/project/src/config
-  #mkdir -p $APP_CONFIG_DIR
-  # cp -rf "$N8N_RENDER_DIR/.n8n/." "$APP_BASE_DIR/config"
-
-  # ###
-  # APP_CONFIG_DIR=/COD/n8n/config
-  # RENDER_N8N_CONFIG_DIR="/opt/render/.n8n"
-
-  RENDER_N8N_CONFIG_DIR="$RENDER_DIR/.n8n"
+  RENDER_N8N_CONFIG_DIR="$RENDER_ROOT/.n8n"
 
   if [ ! -d "$RENDER_N8N_CONFIG_DIR" ]; then
     echo -e "   WARN! not exists dir: $RENDER_N8N_CONFIG_DIR, make dir";
@@ -151,19 +133,20 @@ config_restore() {
     APP_CONFIG_DIR=$APP_INIT_SCRIPT_DIR;
   fi
 
-  echo -e "   Copy config files: $APP_CONFIG_DIR => $RENDER_N8N_CONFIG_DIR";
-  #cp -rf "/COD/n8n/config/." "/opt/render/.n8n/"
-  cp -rf "$APP_CONFIG_DIR/." "$RENDER_N8N_CONFIG_DIR/"
+  #echo -e "   Copy config files: $APP_CONFIG_DIR => $RENDER_N8N_CONFIG_DIR";
+  #cp -rf "$APP_CONFIG_DIR/." "$RENDER_N8N_CONFIG_DIR/"
+  echo -e "   Copy config files: $RENDER_REPO_ROOT/config => $RENDER_N8N_CONFIG_DIR";
+  cp -rf "$APP_INIT_SCRIPT_DIR/." "$RENDER_N8N_CONFIG_DIR/"
 }
 
 # ###   Config save
 config_save() {
   echo -e '   Config save'
-  RENDER_N8N_CONFIG_DIR="$RENDER_DIR/.n8n"
+  RENDER_N8N_CONFIG_DIR="$RENDER_ROOT/.n8n"
 
   if [ -d "$RENDER_N8N_CONFIG_DIR" ]; then
     echo -e "   Copy config files: $RENDER_N8N_CONFIG_DIR => $APP_CONFIG_DIR";
-    # cp -rf "$RENDER_N8N_CONFIG_DIR/." "$APP_CONFIG_DIR/"
+    #cp -rf "$RENDER_N8N_CONFIG_DIR/." "$APP_CONFIG_DIR/"
     cp -rf "$RENDER_N8N_CONFIG_DIR/." "./"
   fi
 
@@ -175,33 +158,16 @@ config_save() {
 #
 init_check() {
 
-  if [ ! -d "$RENDER_DIR" ]; then
-    echo -e "   WARN! not exists dir: $RENDER_DIR, make dir";
-    mkdir -p "$RENDER_DIR";
-    # sudo chown -R $USER:$USER /opt/render/
+  if [ ! -d "$RENDER_ROOT" ]; then
+    echo -e "   WARN! dir '$RENDER_ROOT' not exists, make dir";
+    mkdir -p "$RENDER_ROOT";
+    # sudo chown -R $RENDER_ROOT $USER:$USER
   fi
-
-  if [ -f "$APP_BASE_DIR/.git/index.lock" ]; then rm -f "$APP_BASE_DIR/.git/index.lock"; fi
 
   # #########################################################################################################
   # ###
   #
-  #if [ ! -d "$APP_BASE_DIR" ] ; then
-  #	echo -e "mkdir -p $APP_BASE_DIR";
-  #	mkdir -p "$APP_BASE_DIR";
-  #fi
-  #if [ -d "$APP_BASE_DIR" ] ; then
-  #	# ###   copy n8n distributive
-  #	echo -e "   Copy n8n distr to '$APP_BASE_DIR'";
-  #	mkdir -p "$APP_BASE_DIR"
-  #	cp -rf /c/n8n/. "$APP_BASE_DIR"
-  #	# cp -rf /c/n8n/. /opt/render/project/src
-  #else
-  #	# ###   n8n clone distr
-  #	echo -e "git clone $GIT_REPO"
-  #	# git clone $GIT_REPO
-  #	# git checkout master
-  #fi
+  if [ -f "$APP_BASE_DIR/.git/index.lock" ]; then rm -f "$APP_BASE_DIR/.git/index.lock"; fi
 
 }
 
@@ -216,8 +182,6 @@ init_checkout() {
   if [ "$APP_GITHUB_EMAIL" ]; then
     # ###
     echo -e "Git config user"
-    # git config user.email "valerian.borisovich@gmail.com"
-    # git config user.name "Valerian Borisovich"
     git config user.email $APP_GITHUB_EMAIL
     git config user.name $APP_GITHUB_USER
   # git config credential.helper "store --file ~/.secrets"
@@ -235,10 +199,9 @@ env_load
 env_print
 
 init_check
-# init_checkout
+init_checkout
 
 config_restore
 # config_save
 
-# ### restore directory
-#popd
+
