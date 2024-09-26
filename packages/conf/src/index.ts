10ca2b7d40c0a09ffefaf535d5428327a3cdc535
Valerian <valerian.borisovich@gmail.com>
2024-09-26 07:39:11 +0200

    conf 034

--- packages/conf/src/index.ts
+++ packages/conf/src/index.ts
@@ -1,8 +1,8 @@
 //
-// import { Config, GlobalConfig, Env } from "../../@n8n/config"
-// export { Config, GlobalConfig, Env }
 
+/*
 export * from "../../@n8n/config"
 export * from "../../@n8n/config/src/index"
 export * from "../../@n8n/config/src/configs"
 export * from "../../@n8n/config/src/decorators"
+*/
