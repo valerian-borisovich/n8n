725ef9b01523342ac3e538d5560f1667967d5bdc
Valerian <valerian.borisovich@gmail.com>
2024-09-26 06:40:46 +0200

    conf 030

--- packages/src/index.ts
+++
@@ -1,7 +0,0 @@
-//
-
-//import { Config, Env, Nested } from '../../@n8n/config/src/decorators';
-
-/// import GlobalConfig from "packages/@n8n/config/src/index.ts"
-//import { GlobalConfig } from "../../@n8n/config/src"
-//export { GlobalConfig }
